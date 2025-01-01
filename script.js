e8cee84e4fd9a80594ae2e2e00dfc09373649b4d
stevegwh <sgwheeler@posteo.net>
2025-01-01 15:25:38 +0000

    Updated projects section

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 function scrollToTargetOnLoad(elementId) {
     localStorage.setItem('scrollToElement', elementId);
   }
+
+function showProject(elementId) {
+    console.log("Function called");
+    var projectList = document.getElementsByClassName('project');
+    var alreadyActive = false;
+    var targetElement = document.getElementById(elementId);
+    for (var i = 0; i < projectList.length; i++) {
+        if (projectList[i] == targetElement)
+        {
+            if (projectList[i].style.display == 'block')
+            {
+                alreadyActive = true;
+            }
+        }
+        projectList[i].style.display = 'none';
+    }
+    if (!alreadyActive)
+    {
+        targetElement.style.display = 'block';
+    }
+    
+    var returnElement = document.getElementById("project-return");
+    if (returnElement.style.display = 'none')
+    {
+        returnElement.style.display = 'block';
+    }
+    else
+    {
+        returnElement.style.display = 'none';
+    }
+}
   
   window.addEventListener('DOMContentLoaded', function() {
     var elementId = localStorage.getItem('scrollToElement');
